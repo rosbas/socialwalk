134d898f10af04a8b0cc7e6689b9039ac9794ddd
rosbas <panutatr@hotmail.com>
2019-11-04 01:46:29 +0700

    add createScream function

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # socialwalk
 
 Basically follow this tutorial: https://www.youtube.com/watch?v=m_u6P5k0vP0
-Current progress: 20:16
+Current progress: 26:37
 
 useful command
-Firebase - firebase deploy // after adding command relating to firebase, use this command to upload in to the server.
+Firebase
+
+// after adding command relating to firebase, use this command to upload in to the server.
+
+-   firebase deploy
+
+// local firebase for test, faster than deploy.
+
+-   firebase serve
--- functions/index.js
+++ functions/index.js
@@ -25,3 +25,24 @@ exports.getScreams = functions.https.onRequest((req, res) => {
 		})
 		.catch(err => console.error(err));
 });
+
+exports.createScream = functions.https.onRequest((req, res) => {
+	const newScream = {
+		body: req.body.body,
+		userHandle: req.body.userHandle,
+		createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+	};
+
+	admin
+		.firestore()
+		.collection('screams')
+		.add(newScream)
+		.then(doc => {
+			res.json({ message: `document ${doc.id} created successfully` });
+		})
+		.catch(err => {
+			res.status(500).json({ error: 'something went wong' });
+			console.error(err);
+		});
+});
+//currently having problem with credentails
